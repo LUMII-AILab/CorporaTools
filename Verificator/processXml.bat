97e301b5930650fc21eb11e6abd6d1259165ebc4
Lauma <lauma.pretkalnina@gmail.com>
2012-07-12 19:04:34 +0300

    Pielaboju palaišanas skriptu atbilstoši jaunajai struktūrai.

--- Verificator/processXml.bat
+++ Verificator/processXml.bat
@@ -1,14 +1,17 @@
 :: Assuming that morphology repository can be found in the same folder as Morphocorpus.
 
-copy "..\..\..\..\morphology\dist\Lexicon.xml" "lib\morphology"
-copy "..\..\..\..\morphology\dist\Statistics.xml" "lib\morphology"
+mkdir lib
+mkdir lib\morphology
 
-java -classpath ".;..\..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Balanseetais rez/Balanseetais
+copy "..\..\..\morphology\dist\Lexicon*.xml" "lib\morphology"
+copy "..\..\..\morphology\dist\Statistics.xml" "lib\morphology"
 
-java -classpath ".;..\..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Juridiskais rez/Juridiskais
+java -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Balanseetais rez/Balanseetais
 
-::java -classpath ".;..\..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Ledus rez/Ledus
+java -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Juridiskais rez/Juridiskais
 
-java -classpath ".;..\..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Veestnesis rez/Veestnesis
+::java -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Ledus rez/Ledus
+
+java -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Veestnesis rez/Veestnesis
 
 pause
