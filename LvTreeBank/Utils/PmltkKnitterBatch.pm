46f937fbd18480cabacb5a1ea73804c6dffa2cdd
lauma <lauma.pretkalnina@gmail.com>
2012-11-09 17:49:36 +0200

    Briesmīgi šausmīgs veids, kā kaprīzajam PML-TK kniterim izdzīt cauri vairākus failus.

---
+++ LvTreeBank/Utils/PmltkKnitterBatch.pm
@@ -0,0 +1,52 @@
+#!C:\strawberry\perl\bin\perl -w
+package LvTreeBank::Utils::PmltkKnitterBatch;
+
+use strict;
+use warnings;
+
+use IO::File;
+use IO::Dir;
+
+###############################################################################
+# Batch processing interface for pmltk-1.1.5 knitter. WinXP specific.
+#
+# Input: directory with files to process,
+#		 directory where PML Toolkit is (default is 'pmltk-1.1.5').
+#
+# Developed on Strawberry Perl 5.12.3.0
+# Latvian Treebank project, 2012
+# Lauma Pretkalnina, LUMII, AILab, lauma@ailab.lv
+# Licenced under GPL.
+###############################################################################
+sub runPmltkKnitterBatch
+{
+	autoflush STDOUT 1;
+	if (not @_ or @_ lt 1)
+	{
+		print <<END;
+Batch processing interface for PML Toolkit knitter. Tested on v-1.1.5. WinXP
+specific.
+Input files should be provided as UTF-8.
+
+Params:
+   directory with files to process
+   directory with PML Toolkit [opt, 'pmltk-1.1.5' used otherwise]
+
+Latvian Treebank project, LUMII, 2012, provided under GPL
+END
+		exit 1;
+	}
+	my $dir_name = shift @ARGV;
+	my $pmltk_path = shift @ARGV or 'pmltk-1.1.5';
+	my $dir = IO::Dir->new($dir_name) or die "dir $!";
+
+	while (defined(my $in_file = $dir->read))
+	{
+		if ((! -d "$dir_name/$in_file") and ($in_file =~ /^(.+)\.a$/))
+		{
+			system ("perl $pmltk_path/tools/knit.pl $dir_name/$1.a $dir_name/$1.pml");
+		}
+	}
+}
+
+1;
---
+++ runPmltkKnitterBatch.bat
@@ -0,0 +1,9 @@
+@echo off
+set PERL5LIB=C:\PROGRA~1\tred\dependencies\lib\perl5;C:\PROGRA~1\tred\dependencies\lib\perl5\MSWin32-x86-multi-thread;C:\Program Files\tred\tredlib\libs\fslib;%PERL5LIB%
+@echo on
+
+copy "TrEd extension\lv-treebank\resources\*.xml" "testdata\pmltk-knitter\"
+
+perl -e "use LvTreeBank::Utils::PmltkKnitterBatch; LvTreeBank::Utils::PmltkKnitterBatch::runPmltkKnitterBatch(@ARGV)" testdata/pmltk-knitter pmltk-1.1.5
+
+pause
