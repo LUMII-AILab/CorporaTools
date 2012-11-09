b390d1ec65324d9947b1f31751aed150352a96b2
lauma <lauma.pretkalnina@gmail.com>
2012-11-09 19:33:51 +0200

    Masu apstrādes interfeiss DepPml2Conl.

---
+++ LvTreeBank/Transformations/DepPml2ConllBatch.pm
@@ -0,0 +1,45 @@
+#!C:\strawberry\perl\bin\perl -w
+package LvTreeBank::Transformations::DepPml2ConllBatch;
+
+use strict;
+use warnings;
+
+use LvTreeBank::Transformations::DepPml2Conll;
+use IO::File;
+use IO::Dir;
+
+###############################################################################
+# Batch processing for LvTreeBank::Transformations::DepPml2Conll - if two
+# arguments provided, treat it as directory and process all files in it.
+# Otherwise pass all arguments to DepPml2Conll.
+#
+# Developed on Strawberry Perl 5.12.3.0
+# Latvian Treebank project, 2012
+# Lauma Pretkalnina, LUMII, AILab, lauma@ailab.lv
+# Licenced under GPL.
+###############################################################################
+sub transformFileBatch
+{
+	if (@ARGV eq 2)
+	{
+		my $mode = shift @ARGV;
+		my $dir_name = shift @ARGV;
+		my $dir = IO::Dir->new($dir_name) or die "dir $!";
+		my $infix = $mode ? "nored" : "unlabeled";
+		
+		while (defined(my $in_file = $dir->read))
+		{
+			if ((! -d "$dir_name/$in_file") and ($in_file =~ /^(.+)\.(pml|xml)$/))
+			{
+				LvTreeBank::Transformations::DepPml2Conll::transformFile (
+					$mode, $dir_name, $in_file, "$1-$infix.conll");
+			}
+		}
+
+	}
+	else
+	{
+		LvTreeBank::Transformations::DepPml2Conll::transformFile (@ARGV);
+	}
+}
+1;
---
+++ runDepPml2ConllBatch.bat
@@ -0,0 +1,5 @@
+perl -e "use LvTreeBank::Transformations::DepPml2ConllBatch; LvTreeBank::Transformations::DepPml2ConllBatch::transformFileBatch(@ARGV)" 0 testdata\DepPml2Conll
+
+perl -e "use LvTreeBank::Transformations::DepPml2ConllBatch; LvTreeBank::Transformations::DepPml2ConllBatch::transformFileBatch(@ARGV)" 1 testdata\DepPml2Conll
+
+pause
