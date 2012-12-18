e5e0cabc302fdbd2aee4201ca9ddbc0680577a71
Lauma <lauma@ailab.lv>
2012-12-19 01:34:45 +0200

    Invocation example.

--- MorphoVerificator/runMorphoVerificatorSample.bat
+++ MorphoVerificator/runMorphoVerificatorSample.bat
@@ -6,14 +6,8 @@ mkdir lib\morphology
 copy "..\..\..\morphology\dist\Lexicon*.xml" "lib\morphology"
 copy "..\..\..\morphology\dist\Statistics.xml" "lib\morphology"
 
-java -Xmx1500m -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Balanseetais rez/Balanseetais
+java -Xmx1500m -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator corpus1_source corpus1_result
 
-java -Xmx1500m -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Juridiskais rez/Juridiskais
-
-::java -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Ledus rez/Ledus
-
-java -Xmx1500m -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Veestnesis rez/Veestnesis
-
-::java -Xmx1500m -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator data/Treebank rez/Treebank
+java -Xmx1500m -classpath ".;..\..\..\morphology\dist\morphology.jar" lv.morphology.corpora.CorpusVerificator corpus2_source corpus2_result
 
 pause
