59f9420540526a5ab8ab1df1002ff0bac8d37cc7
Lauma <lauma@ailab.lv>
2013-03-01 14:49:17 +0200

    Corrected use cases, accordingly to provided test data.

--- runDepPml2ConllBatch.bat
+++ runDepPml2ConllBatch.bat
@@ -1,3 +1,5 @@
-perl -e "use LvCorporaTools::TreeTransformations::DepPml2ConllBatch qw(transformFileBatch); transformFileBatch(@ARGV)" 0 knitted none full 1
+perl -e "use LvCorporaTools::TreeTransformations::DepPml2ConllBatch qw(transformFileBatch); transformFileBatch(@ARGV)" 0 testdata\DepPml2Conll none full
+
+perl -e "use LvCorporaTools::TreeTransformations::DepPml2ConllBatch qw(transformFileBatch); transformFileBatch(@ARGV)" 1 testdata\DepPml2Conll none full
 
 pause
--- runTreebankSplitter.bat
+++ runTreebankSplitter.bat
@@ -1,3 +1,5 @@
-perl -e "use LvCorporaTools::TestDataSelector::SplitTreebank qw(splitCorpus); splitCorpus(@ARGV)" knitted\res 10 0
+perl -e "use LvCorporaTools::TestDataSelector::SplitTreebank qw(splitCorpus); splitCorpus(@ARGV)" testdata\TreebankSplitter 0.2 0
+
+perl -e "use LvCorporaTools::TestDataSelector::SplitTreebank qw(splitCorpus); splitCorpus(@ARGV)" testdata\TreebankSplitter 4 0
 
 pause
