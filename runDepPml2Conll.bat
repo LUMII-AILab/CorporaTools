401f10e27a3df93be1e690b1281062154910e0d5
Lauma <lauma@ailab.lv>
2013-03-01 15:07:10 +0200

    Test case using CoNLL 2009 switch.

--- runDepPml2Conll.bat
+++ runDepPml2Conll.bat
@@ -1,5 +1,5 @@
-perl -e "use LvCorporaTools::TreeTransformations::DepPml2Conll qw(transformFile); transformFile(@ARGV)" 0 testdata\DepPml2Conll zeens-dep.xml none full zeens-unlabeled.conll
+perl -e "use LvCorporaTools::TreeTransformations::DepPml2Conll qw(transformFile); transformFile(@ARGV)" 0 testdata\DepPml2Conll zeens-dep.xml none full zeens-unlabeled.conll 0
 
-perl -e "use LvCorporaTools::TreeTransformations::DepPml2Conll qw(transformFile); transformFile(@ARGV)" 1 testdata\DepPml2Conll zeens-dep.xml none full zeens-omit-reductions.conll
+perl -e "use LvCorporaTools::TreeTransformations::DepPml2Conll qw(transformFile); transformFile(@ARGV)" 1 testdata\DepPml2Conll zeens-dep.xml none full zeens-omit-reductions.conll 1
 
 pause
