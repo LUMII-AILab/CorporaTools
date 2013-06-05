f742f6c1a24c5f1db829717ce8803426234c6f99
lauma <lauma.pretkalnina@gmail.com>
2013-06-05 17:32:12 +0300

    For the sake of simplicity.

--- runDepPml2Conll.bat
+++ runDepPml2Conll.bat
@@ -1,7 +1,13 @@
-REM Unlabeled trees in "small" CoNLL with default POSTAG and CPOSTAG.
+REM Unlabeled trees in "small" CoNLL with default POSTAG and CPOSTAG; single file.
 perl -e "use LvCorporaTools::FormatTransf::DepPml2Conll qw(transformFile); transformFile(@ARGV)" testdata\DepPml2Conll zeens-dep.xml 0 zeens-unlabeled.conll 0
 
-REM Labeled trees in "large" CoNLL format with custom POSTAG and CPOSTAG.
+REM Labeled trees in "large" CoNLL format with custom POSTAG and CPOSTAG; single file.
 perl -e "use LvCorporaTools::FormatTransf::DepPml2Conll qw(transformFile POSTAG CPOSTAG); $POSTAG = 'FULL'; $CPOSTAG = 'FIRST'; transformFile(@ARGV)" testdata\DepPml2Conll zeens-dep.xml 1 zeens-omit-reductions.conll 1
+
+REM Unlabeled trees in "small" CoNLL with default POSTAG and CPOSTAG; entire folder.
+perl -e "use LvCorporaTools::FormatTransf::DepPml2Conll qw(processDir); processDir(@ARGV)" testdata\DepPml2Conll 0 0
+
+REM Labeled trees in "small" CoNLL format with custom POSTAG and CPOSTAG; entire folder.
+perl -e "use LvCorporaTools::FormatTransf::DepPml2Conll qw(processDir POSTAG CPOSTAG); $POSTAG = 'FULL'; $CPOSTAG = 'FIRST'; processDir(@ARGV)" testdata\DepPml2Conll 1 0
 
 pause
