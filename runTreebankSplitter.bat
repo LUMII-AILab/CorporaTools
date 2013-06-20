02ff157813e366037873687b9cbf6d3b0e60f515
lauma <lauma.pretkalnina@gmail.com>
2013-06-20 19:55:55 +0300

    Description.

--- runTreebankSplitter.bat
+++ runTreebankSplitter.bat
@@ -1,7 +1,10 @@
+REM To seperate 1/5 into one data set and 2/5 into other (seed: 0).
 perl -e "use LvCorporaTools::DataSelector::SplitTreebank qw(splitCorpus); splitCorpus(@ARGV)" testdata\TreebankSplitter 0.2 0
 
+REM To make 4 aproximetly even-sized datasets (seed: 0).
 perl -e "use LvCorporaTools::DataSelector::SplitTreebank qw(splitCorpus); splitCorpus(@ARGV)" testdata\TreebankSplitter 4 0
 
+REM To concatenate multiple CoNLL files.
 perl -e "use LvCorporaTools::DataSelector::SplitTreebank qw(splitCorpus); splitCorpus(@ARGV)" testdata\TreebankSplitter 1
 
 pause
