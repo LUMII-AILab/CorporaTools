2e53d9fd007749620432017b3185b528f5bc4bfd
lauma <lauma.pretkalnina@gmail.com>
2013-12-02 17:59:06 +0200

    Syntax typo.

--- ParserTools/runConllStats.bat
+++ ParserTools/runConllStats.bat
@@ -1,4 +1,4 @@
 REM At least one flag must be present.
-java -classpath ".lib/*" lv.ailab.parser.tools.ConllStats corpus.conll --nonproj --multiroot
+java -classpath ".;lib/*" lv.ailab.parser.tools.ConllStats corpus.conll --nonproj --multiroot
 
 pause
