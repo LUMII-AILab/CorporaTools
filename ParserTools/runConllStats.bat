edd3ac8775f38a1600c3dde1ea5201b8abd25017
lauma <lauma.pretkalnina@gmail.com>
2013-12-02 17:48:22 +0200

    More meaning full invocation example.

--- ParserTools/runConllStats.bat
+++ ParserTools/runConllStats.bat
@@ -1,8 +1,4 @@
-java -classpath ".;maltparser-1.7.jar;lib/*" lv.ailab.parser.tools.ConllStats sem-pos-devel-20131007.conll --nonproj --multiroot
-java -classpath ".;maltparser-1.7.jar;lib/*" lv.ailab.parser.tools.ConllStats sem-pos-test-20131007.conll --nonproj --multiroot
-
-java -classpath ".;maltparser-1.7.jar;lib/*" lv.ailab.parser.tools.ConllStats sem-phdep-pos-devel-LETA-20131007.conll --nonproj --multiroot
-java -classpath ".;maltparser-1.7.jar;lib/*" lv.ailab.parser.tools.ConllStats sem-phdep-pos-devel-neNeiburga-20131007.conll --nonproj --multiroot
-java -classpath ".;maltparser-1.7.jar;lib/*" lv.ailab.parser.tools.ConllStats sem-phdep-pos-devel-Neiburga-20131007.conll --nonproj --multiroot
+REM At least one flag must be present.
+java -classpath ".lib/*" lv.ailab.parser.tools.ConllStats corpus.conll --nonproj --multiroot
 
 pause
