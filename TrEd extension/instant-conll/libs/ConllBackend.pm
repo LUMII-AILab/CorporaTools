2b35cc7a0e466d4ba72452084fb270f72b1eed41
lauma <lauma.pretkalnina@gmail.com>
2014-04-02 23:09:29 +0300

    Moved PML schema to separate file.

--- TrEd extension/instant-conll/libs/ConllBackend.pm
+++ TrEd extension/instant-conll/libs/ConllBackend.pm
@@ -5,6 +5,7 @@ use strict;
 use Treex::PML::Factory;
 use Treex::PML::Document;
 use Treex::PML::Node;
+use Treex::PML;
 
 #use Carp::Always;
 
@@ -48,14 +49,15 @@ sub read
 	$fsfile->changeBackend('ConllBackend');
 	$fsfile->changeFileFormat('CoNLL-2007');
 	my $schema = Treex::PML::Factory->createPMLSchema({
-		'string' => &_get_schema});
-		#'filename' => 'libs/conll2007schema.xml'});
+		'filename' => Treex::PML::FindInResourcePaths ('conll2007schema.xml')});
 	$fsfile->changeMetaData('schema', $schema);
 	#my $fsthingy = Treex::PML::Factory->createFSFormat([
 	#	'@N ord', '@V form', '@K lemma', '@K cpostag', '@K postag',
 	#	'@K deprel', '@K phead', '@K pdeprel']);
 	#$fsfile->changeFS($fsthingy);
 	$fsfile->changeFS(&_get_simple_FSformat($schema));
+	
+	print Treex::PML::FindInResourcePaths ('conll2007schema.xml');
 
 	# Fetch imput data.
 	my @rows = <$filehandle>;
@@ -198,59 +200,6 @@ sub _get_simple_FSformat
 	}
 	
 	return $fs;
-}
-
-# PML schema definition for trees in CoNLL 2007 files.
-# TODO: move this to a seperate data file. How to link to such file?
-sub _get_schema
-{
-	my $schema = <<END;
-<?xml version="1.0" encoding="utf-8"?>
-<pml_schema
-  xmlns="http://ufal.mff.cuni.cz/pdt/pml/schema/"
-  version="1.1">
-  <revision>0.1</revision>
-  <description>Schema for CoNLL-2007 syntax data (10 column format).</description>
-
-  <root name="conlldata" type="treelist.type"/>
-
-  <type name="treelist.type">
-    <structure>
-      <member name="trees" role="#TREES" required="1">
-        <list type="root.type" ordered="1"/>
-      </member>
-    </structure>
-  </type>
-  
-  <type name="root.type"> <!-- Root.-->
-	<structure role="#NODE" name="node">
-      <member name="ord" role="#ORDER" required="1"><constant>0</constant></member>
-      <member name="children" role="#CHILDNODES">
-        <list type="node.type" ordered="1"/>
-      </member>	  
-	</structure>
-  </type>
-  
-  <type name="node.type"> <!-- Arbitrary node. -->
-	<structure role="#NODE" name="node">
-	  <member name="form" required="1"><cdata format="string"/></member>
-      <member name="ord" role="#ORDER" required="1"><cdata format="positiveInteger"/></member>
-	  <member name="lemma"><cdata format="string"/></member>
-	  <member name="cpostag"><cdata format="any"/></member>
-	  <member name="postag"><cdata format="any"/></member>
-	  <member name="feats"><list odered='0'><cdata format="any"/></list></member>
-	  <member name="deprel"><cdata format="any"/></member>
-	  <member name="phead"><cdata format="nonNegativeInteger"/></member>
-	  <member name="pdeprel"><cdata format="any"/></member>
-      <member name="children" role="#CHILDNODES">
-        <list type="node.type" ordered="1"/>
-      </member>	  
-	</structure>
-  </type>
-    
-</pml_schema>
-END
-	return $schema;
 }
 
 1;
