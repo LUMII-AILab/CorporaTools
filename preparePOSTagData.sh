fc11f2c84d5e0545427b62e6d8ac234b7af7770d
PeterisP <peterisp@gmail.com>
2016-05-10 19:04:37 +0300

    Updated scripts to prepare morphocorpus for training tagger, predefined split in train/dev/test

--- preparePOSTagData.sh
+++ preparePOSTagData.sh
@@ -11,6 +11,10 @@ fi
 pmlFolder="$sourceFolder/Merged"
 rm -rf $pmlFolder
 mkdir $pmlFolder
+mkdir $pmlFolder/dev
+mkdir $pmlFolder/test
+mkdir $pmlFolder/train
+
 # This subset of corpora is double-checked and usable for tagger training
 cp $sourceFolder/Balanseetais/Jaunaakais/*.m $pmlFolder
 cp $sourceFolder/Latvijas\ Veestnesis/Jaunaakais/*.m $pmlFolder
@@ -21,18 +25,30 @@ treebankFolder="../Treebank/Corpora"
 # cp $treebankFolder/Corpora/**/*.m $pmlFolder
 find $treebankFolder/ -name "*.m" -exec cp {} $pmlFolder ";"
 
+# We use a predefined file split between train/dev/test
+for file in $(<LvCorporaTools/corpus_devset.txt); do 
+	# echo "copying $file to devset"
+	mv "$pmlFolder/$file.m" "$pmlFolder/dev";
+done
+for file in $(<LvCorporaTools/corpus_testset.txt); do 
+	# echo "copying $file to testset"
+	mv "$pmlFolder/$file.m" "$pmlFolder/test";
+done
+for file in $pmlFolder/*.m; do 
+	mv "$file" "$pmlFolder/train/";
+done
+
+# If the files would need splitting between dev/test/train, then the following lines would do it
+
+# perl -e "use LvCorporaTools::DataSelector::SplitMorphoData; LvCorporaTools::DataSelector::SplitMorphoData::splitCorpus(@ARGV)" "$pmlFolder" 0.2 0
+# mv "$pmlFolder/dev" "$pmlFolder/train"
+# mv "$pmlFolder/test" "$pmlFolder/devtest"
+# perl -e "use LvCorporaTools::DataSelector::SplitMorphoData; LvCorporaTools::DataSelector::SplitMorphoData::splitCorpus(@ARGV)" "$pmlFolder/devtest" 0.5 0
+# mv "$pmlFolder/devtest/dev" "$pmlFolder/"
+# mv "$pmlFolder/devtest/test" "$pmlFolder/"
+# rm -rf "$pmlFolder/devtest"
+
 echo "Converting $pmlFolder"
-
-perl -e "use LvCorporaTools::DataSelector::SplitMorphoData; LvCorporaTools::DataSelector::SplitMorphoData::splitCorpus(@ARGV)" "$pmlFolder" 0.2 0
-
-mv "$pmlFolder/dev" "$pmlFolder/train"
-mv "$pmlFolder/test" "$pmlFolder/devtest"
-
-perl -e "use LvCorporaTools::DataSelector::SplitMorphoData; LvCorporaTools::DataSelector::SplitMorphoData::splitCorpus(@ARGV)" "$pmlFolder/devtest" 0.5 0
-
-mv "$pmlFolder/devtest/dev" "$pmlFolder/"
-mv "$pmlFolder/devtest/test" "$pmlFolder/"
-rm -rf "$pmlFolder/devtest"
 
 for n in $(find "$pmlFolder/train" -type f -name "*.m")
 do
@@ -55,8 +71,8 @@ find "$pmlFolder/test" -type f -name "*.txt" -exec cat {} > "$pmlFolder/test.txt
 rm -rf "$pmlFolder/train"
 rm -rf "$pmlFolder/dev"
 rm -rf "$pmlFolder/test"
-rm $pmlFolder/*.m
 
 cat "$pmlFolder/train.txt" "$pmlFolder/dev.txt" "$pmlFolder/test.txt" > "$pmlFolder/all.txt"
+cat "$pmlFolder/train.txt" "$pmlFolder/dev.txt" > "$pmlFolder/train_dev.txt"
 
 echo "Done!"
