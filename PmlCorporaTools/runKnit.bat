18338a0a34b8f055897eea3ffd112cbf80ff7967
lauma <lauma.pretkalnina@gmail.com>
2017-04-07 15:32:25 +0300

    More "knitting" examples + corrected schema paths.

--- PmlCorporaTools/runKnit.bat
+++ PmlCorporaTools/runKnit.bat
@@ -1,8 +1,14 @@
 ::copy "TrEd extension\lv-treebank\resources\*.xml" "testdata\Knit\"
-REM Knit-in single file.
-perl -e "use LvCorporaTools::PMLUtils::Knit qw(knit); knit(@ARGV)" testdata/Knit zeens.a zeens.pml "TrEd extension/lv-treebank/resources"
+REM Knit-in single A file.
+perl -e "use LvCorporaTools::PMLUtils::Knit qw(knit); knit(@ARGV)" testdata/Knit zeens.a zeens-a.pml "../TrEd extension/lv-treebank/resources"
 
-REM Process a folder.
-perl -e "use LvCorporaTools::PMLUtils::Knit qw(processDir); processDir(@ARGV)" testdata/Knit/ a "TrEd extension/lv-treebank/resources"
+REM Knit-in single M file.
+perl -e "use LvCorporaTools::PMLUtils::Knit qw(knit); knit(@ARGV)" testdata/Knit zeens.m zeens-m.pml "../TrEd extension/lv-treebank/resources"
+
+REM Process a folder with filesets containing A files.
+perl -e "use LvCorporaTools::PMLUtils::Knit qw(processDir); processDir(@ARGV)" testdata/Knit/ a "../TrEd extension/lv-treebank/resources"
+
+REM Process a folder with filesets containing M files.
+perl -e "use LvCorporaTools::PMLUtils::Knit qw(processDir); processDir(@ARGV)" testdata/Knit/ m "../TrEd extension/lv-treebank/resources"
 
 pause
