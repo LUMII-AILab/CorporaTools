85a726ce8af53b0b86d364565ee3ecc847336935
lauma <lauma.pretkalnina@gmail.com>
2017-06-27 16:45:16 +0300

    Docs.

--- PmlCorporaTools/convertLvtbToUd_sample.bat
+++ PmlCorporaTools/convertLvtbToUd_sample.bat
@@ -1,5 +1,5 @@
 
-:: Prepare LVTB data for transforming
+:: Prepare LVTB data for transforming (your data goes into folder "data")
 ::perl LvCorporaTools/UIs/TreeTransformatorUI.pm --dir data --collect --ord mode=TOKEN --knit
 
 :: Copy all the data from data/knitted to the LVTB2UD data folder
@@ -8,7 +8,7 @@
 :: Copy results from CorporaTools/LVTB2UD/out/production/data/conll-u to data/complete
 :: Create data splits in folders data/train, data/test, data/dev
 
-:: Fold everything neatly
+:: Fold everything neatly (assumed files are separated in folders "train", "test", "dev" and "complete")
 ::perl LvCorporaTools/UIs/TreeTransformatorUI.pm --dir data/train --fold p=1 name=lv-ud-train
 ::perl LvCorporaTools/UIs/TreeTransformatorUI.pm --dir data/test --fold p=1 name=lv-ud-test
 ::perl LvCorporaTools/UIs/TreeTransformatorUI.pm --dir data/dev --fold p=1 name=lv-ud-dev
