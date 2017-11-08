d916a07b330e7301f977d3317dfffae1ae81177f
lauma <lauma.pretkalnina@gmail.com>
2017-11-08 17:00:09 +0200

    Simple batch processing added.

--- PmlCorporaTools/LvCorporaTools/GenericUtils/ApplyXSLT.pm
+++ PmlCorporaTools/LvCorporaTools/GenericUtils/ApplyXSLT.pm
@@ -3,20 +3,48 @@ package LvCorporaTools::GenericUtils::ApplyXSLT;
 
 #use utf8;
 use strict;
+use warnings;
+
+use File::Path;
+use IO::Dir;
+use XML::LibXSLT;
+use XML::LibXML;
 
 use Exporter();
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw(applyXSLT1_0);
+our @EXPORT_OK = qw(applyXSLT1_0 transformDir);
+
+sub transformDir
+{
+	print "Usage: data_dir xslt_transform_file output_dir [.input_extension] [.output_extension]\n"
+		if (@_ < 3 or @) > 5);
+	my $inputDir = shift @_;
+	my $transf = shift @_;
+	my $output = shift @_;
+	my $inputExt = (shift @_ or '');
+	my $outputExt = (shift @_ or '');
+	my $dir = IO::Dir->new($inputDir) or die "dir $!";
+	mkpath($output);
+
+	my $baddies = 0;
+	while (defined(my $inFile = $dir->read))
+	{
+		if ((-f "$inputDir/$inFile") and ($inFile =~ /^(.*?)$inputExt$/))
+		{
+			local $SIG{__WARN__} = sub { $baddies++; warn $_[0] }; # This magic makes eval count warnings.
+			applyXSLT1_0("$inputDir/$inFile", $transf, "$output/$1$outputExt");
+		}
+	}
+	print "$baddies failed.\n" if ($baddies);
+}
 
 sub applyXSLT1_0
 {
-	use XML::LibXSLT;
-	use XML::LibXML;
 
 	print "Usage: xml_data_file xslt_transform_file output_file\n"
-		if (@ARGV != 3);
+		if (@_ != 3);
 
-	my ($input, $transf, $output) = @ARGV;  
+	my ($input, $transf, $output) = @_;
 	my $parser = XML::LibXML->new();
 	my $xslt = XML::LibXSLT->new();
 	  
--- PmlCorporaTools/runPmlMToPlain.bat
+++ PmlCorporaTools/runPmlMToPlain.bat
@@ -1,3 +1,7 @@
-perl -e "use LvCorporaTools::GenericUtils::ApplyXSLT qw(applyXSLT1_0); applyXSLT1_0(@ARGV)" testdata\PmlMToPlain\zeens.m LvCorporaTools\FormatTransf\pmlM2plain.xsl zeens.txt
+REM Single file.
+perl -e "use LvCorporaTools::GenericUtils::ApplyXSLT qw(applyXSLT1_0); applyXSLT1_0(@ARGV)" testdata\PmlMToPlain\zeens.m LvCorporaTools\FormatTransf\pmlM2plain.xsl testdata\PmlMToPlain\zeens.txt
+
+REM Multiple files in folder.
+perl -e "use LvCorporaTools::GenericUtils::ApplyXSLT qw(transformDir); transformDir(@ARGV)" testdata\PmlMToPlain LvCorporaTools\FormatTransf\pmlM2plain.xsl testdata\PmlMToPlain\res .m .vert
 
 pause
