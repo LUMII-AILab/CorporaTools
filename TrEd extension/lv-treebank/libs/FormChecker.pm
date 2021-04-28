fba2067a701dfd00e503a018d7723a2a463f6c5d
Lauma <lauma.pretkalnina@gmail.com>
2021-04-28 16:18:15 +0300

    Precizēta noliegto formu pārbaude.

--- TrEd extension/lv-treebank/libs/FormChecker.pm
+++ TrEd extension/lv-treebank/libs/FormChecker.pm
@@ -14,8 +14,10 @@ sub checkFormByTag
 	my @errors = ();
 	
 	# TODO update accordingly linguist feedback.
-	push @errors, 'Form must start with \'ne\'!' if ($tag =~ /^v..[^p].{6}y.*/ and $form !~ /^ne/);
-	push @errors, 'Form must start with \'ne\'!' if ($tag =~ /^v..p.{8}y.*/ and $form !~ /^ne/);
+	push @errors, 'Form must start with \'ne\'!' if ($tag =~ /^v..[icm].{6}y.*/ and $form !~ /^[Nn]([Ee]|[Aa][Vv]$)/);
+	push @errors, 'Form must start with \'ne\'!' if ($tag =~ /^v..[rn].{6}y.*/ and $form !~ /^[Nn][Ee]/);
+	push @errors, 'Form must start with \'ne\'!' if ($tag =~ /^v..p.{8}y.*/ and $form !~ /^[Nn][Ee]/);
+	push @errors, 'Form must start with \'jā\'!' if ($tag =~ /^v..d.*/ and $form !~ /^[Jj][Āā]/);
 	
 	#print Dumper ({'Form' => $form, 'Tag' => $tag, 'Result' => \@errors});
 	
