2c8b502964a02c7084f709e1dd876536a61338cf
lauma <lauma@users.noreply.github.com>
2025-10-30 16:00:43 +0200

    Skripts LTG pārveidotāja izsaukšanai.

---
+++ LVTB2UD/resources/runUniversalizer-LTG.bat
@@ -0,0 +1,4 @@
+REM chcp 1257
+::java -cp "./lib/*;." lv.ailab.lvtb.universalizer.LvtbToUdUI add_node_ids=true omit_whole_files=true
+java -cp "./lib/*;." lv.ailab.lvtb.universalizer.LvtbToUdUI add_node_ids=true omit_whole_files=true latgalian=true
+@pause
